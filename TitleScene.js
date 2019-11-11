424ca7b137418a5ec7b433da560f4dd829edbd95
Lucas <lucas.taddei@unil.ch>
2019-11-11 18:01:39 +0100

    start

--- TitleScene.js
+++ TitleScene.js
@@ -2,4 +2,7 @@ class TitleScene extends Phaser.Scene {
     constructor(){
         super("title");
     }
+    create(){
+    this.add.text(20, 20, "Fake News, Real Bruise");
+}
 }
--- game.js
+++ game.js
@@ -5,5 +5,5 @@ var config = {
     width: 800,
     height: 600,
     backgroundColor: "#65FF99",
-    scene: [EndScene,TitleScene,ResultScene,NotAVideoGameScene,GameplayScene]
+    scene: [TitleScene,GameplayScene,ResultScene,NotAVideoGameScene,EndScene]
 }
