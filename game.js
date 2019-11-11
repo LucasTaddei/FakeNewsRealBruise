81cf407356a2581904d219f3d2e6bf65e5c9b19c
Lucas <lucas.taddei@unil.ch>
2019-11-11 17:18:56 +0100

    scenes

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
 window.onload=function(){
-    var game = new Phaser.Game();
+    var game = new Phaser.Game(config);
+}
+var config = {
+    width: 800,
+    height: 600,
+    backgroundColor: 0x000000,
 }
