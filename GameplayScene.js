5ec3dca9bafdd6e8b94355eada97a4032cd6fc58
Lucas <lucas.taddei@unil.ch>
2019-11-14 13:22:28 +0100

    music works hooray

--- GameplayScene.js
+++ GameplayScene.js
@@ -5,9 +5,12 @@ class GameplayScene extends Phaser.Scene {
 
     preload(){
         this.load.image("background", "assets/images/network.jpg")
+        this.load.audio("mainsong", "assets/sounds/realbruise.m4a")
     }
 
     create(){
+        let mainsong = this.sound.add("mainsong");
+        mainsong.play();
         this.background = this.add.image(0,0, "background");
         this.background.setOrigin(0,0);
         this.add.text(20, 20, "Game Scene", {font: "25px Arial", fill: "black"})
