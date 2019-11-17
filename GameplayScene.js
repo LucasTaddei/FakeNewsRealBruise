47417b66c49d0977ed9661d57e759128e28c8dfb
LeaKiller <lea.keller.opp@gmail.com>
2019-11-17 13:13:56 +0100

    Add combo for sharing news

--- GameplayScene.js
+++ GameplayScene.js
@@ -13,7 +13,8 @@ class gameplayScene extends Phaser.Scene {
         // définition des compteurs de score
         this.catchedArrows = 0;
         this.missedArrows = 0;
-        this.sharedNews= 0;
+        this.consecutiveArrows = 0;
+        this.sharedNews = 0;
 
         this.scoreLabel;
         this.failLabel;
@@ -79,9 +80,9 @@ class gameplayScene extends Phaser.Scene {
 
         this.scoreLabel = this.add.text(20, 60, this.catchedArrows, {font: "25px Arial", fill: "white"});
         this.failLabel = this.add.text(20, 90, this.catchedArrows, {font: "25px Arial", fill: "red"});
-        this.sharedLabel = this.add.text(20, 120, this.sharedNews, { font: "25px Arial", fill: "green"});
+        this.sharedLabel = this.add.text(20, 120, this.sharedNews, {font: "25px Arial", fill: "green"});
 
-        var label = this.add.text(0, 0, '', { font: "48px Arial Black", fill: "#c51b7d" });
+        var label = this.add.text(0, 0, '', {font: "48px Arial Black", fill: "#c51b7d"});
         label.setStroke('#de77ae', 8);
 
         // création des 4 zones de collision des flèches
@@ -120,20 +121,26 @@ class gameplayScene extends Phaser.Scene {
 
                 if (isLeftKeyPressed && currentArrow.name == "left") {
                     this.catchedArrows++;
+                    this.consecutiveArrows++
                     this.removeArrow(currentArrow);
                 } else if (isUpKeyPressed && currentArrow.name == "up") {
                     this.catchedArrows++;
+                    this.consecutiveArrows++
                     this.removeArrow(currentArrow);
                 } else if (isDownKeyPressed && currentArrow.name == "down") {
                     this.catchedArrows++;
+                    this.consecutiveArrows++
                     this.removeArrow(currentArrow);
                 } else if (isRightKeyPressed && currentArrow.name == "right") {
                     this.catchedArrows++;
+                    this.consecutiveArrows++
                     this.removeArrow(currentArrow);
                 }
 
-                if (this.catchedArrows % 5 == 0){
+                // enregistrement des combos
+                if (this.consecutiveArrows == 5){
                     this.sharedNews++;
+                    this.consecutiveArrows = 0;
                 }
                 
             }
@@ -141,6 +148,7 @@ class gameplayScene extends Phaser.Scene {
             // suppression de la flèche du tableau une fois au-dehors de la zone pour éviter une saturation de la mémoire
             if (currentArrow.y > 720){
                 this.missedArrows++;
+                this.consecutiveArrows = 0;
                 this.removeArrow(currentArrow);
             }
 
