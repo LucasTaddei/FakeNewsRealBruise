57678473ab936448484effba0ba646a078210b63
Kykka90 <44426484+Kykka90@users.noreply.github.com>
2019-11-18 17:15:17 +0100

    Font prep

--- TitleScene.js
+++ TitleScene.js
@@ -5,14 +5,19 @@ class titleScene extends Phaser.Scene {
     }
 
     preload(){
+        
         this.load.image('start','assets/UIpack/PNG/grey_button02.png');
+        this.load.image('logo','assets/images/LOGO.png')
     }
 
    create(){
     var start = this.add.image(640,500,'start').setInteractive();
-    var startText = this.add.text(640,500, "Start", {font: "20px Times", fill: "black"}).setOrigin(0.5);
-    var titleText = this.add.text(640, 200,"Fake News, Real Bruise", {font: "60px Times", fill: "black"}).setOrigin(0.5);
-       
+    var logo = this.add.image(640,400,'logo').setScale(0.6);
+    var startText = this.add.text(640,500, "Start", {font: "20px jack", fill: "black"}).setOrigin(0.5);
+    var titleTextLine1 = this.add.text(200, 100,"Fake News", {font: '90px jack', fill: "black"});
+    var titleTextLine2 = this.add.text(550, 170,"Real Bruise", {font: '90px jack', fill: "black"});
+    var gameExplanation = this.add.text(640, 450, "Share the posts swiftly and in rythms", {font: '50px jack', fill:white})
+        
     start.on('pointerdown', () => this.scene.start("gameplay"));
     }
 }
