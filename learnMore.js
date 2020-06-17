48c618eec716dab339fca880a2c12e56f37f8929
MarieG92 <44426548+MarieG92@users.noreply.github.com>
2020-06-17 13:09:49 +0200

    Update learnMore.js

--- learnMore.js
+++ learnMore.js
@@ -22,8 +22,8 @@ class learnMore extends Phaser.Scene {
     var skyline = this.add.image(640, 360, 'skyline').setOrigin(0.5);
 
     // ajouter texte phase 1
-    var learnMoreTitle1 = this.add.text(640, 150, "Qu'est-ce qu'une fake news", {font: '55px jack', fill: '#112b1a'}).setOrigin(0.5).setAlpha(0);
-    var learnMoreText1 = this.add.text(640, 350, 'C’est une information volontairement mensongère et qui est destinée à tromper ou à manipuler la personne qui la lira. L’idée d’intention, de manipulation, est importante.\n Dans le but de manipuler et dans une finalité idéologique ou une volonté de faire le buzz, la force des fake news est de mimer une véritable information. On copie de vrais sites en formatant les infos pour qu’elles puissent passer pour vraies.\n Les fake news utilisent des ressorts très émotionnels qui favorisent les clics, le partage, l’engagement.', {font: '25px imperator', fill: '#112b1a', lineSpacing: 10}).setOrigin(0.5);
+    var learnMoreTitle1 = this.add.text(640, 150, "What is a fake news ?", {font: '55px jack', fill: '#112b1a'}).setOrigin(0.5).setAlpha(0);
+    var learnMoreText1 = this.add.text(640, 350, 'It is information that is deliberately misleading and is intended to deceive or manipulate the person reading it. The idea of intent, of manipulation, is important.\n  In order to manipulate and with an ideological purpose or a desire to create a buzz, the strength of fake news is to mimic real information. We copy real sites, formatting the news to make it look real.\n Fake news uses very emotional springs that encourage clicks, sharing and engagement.', {font: '25px imperator', fill: '#112b1a', lineSpacing: 10}).setOrigin(0.5);
     learnMoreText1.setWordWrapWidth(1000, false).setAlign('center').setAlpha(0);
     var nextButton1 = this.add.text(640,570, "Next", {font: "60px jack", fill: "#112b1a"}).setOrigin(0.5).setInteractive().setAlpha(0);
     this.tweens.add({
