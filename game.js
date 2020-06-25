c819285874f8169966dac1b5107caa5f9b218169
LeaKiller <lea.keller.opp@gmail.com>
2020-06-25 15:51:17 +0200

    Correction latence audio selon navigateur

--- game.js
+++ game.js
@@ -1,4 +1,11 @@
-window.onload=function(){
+// fonction pour récupérer le navigateur du jeu afin de configurer le paramètre disableWebAudio en conséquence
+var isSafari;
+
+window.onload=function() {
+
+    // https://stackoverflow.com/a/23522755
+    isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
     var game = new Phaser.Game(config);
 }
 
@@ -7,6 +14,5 @@ var config = {
     height: 720,
     backgroundColor: "#65FF99",
     scene: [titleScene, gameplayScene, pauseScene, ifNoClicks, resultScene, learnMore, endScene],
-    audio: {disableWebAudio: false}
-    
+    audio: {disableWebAudio: !isSafari}
 }
